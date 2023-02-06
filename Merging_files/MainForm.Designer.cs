ee35b4f40635ed977d91f1807136b4c929803fbf
Arkadiusz <Arkadiusz@DESKTOP-UI9D9F2>
2023-02-06 23:16:25 +0100

    Dodaj pliki projektów.

---
+++ Merging_files/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Merging_files
+{
+    partial class MergingFiles
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddFile = new System.Windows.Forms.Button();
+            this.opdAddInput = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnDleleteAll = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddFile
+            // 
+            this.btnAddFile.Location = new System.Drawing.Point(458, 25);
+            this.btnAddFile.Name = "btnAddFile";
+            this.btnAddFile.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFile.TabIndex = 0;
+            this.btnAddFile.Text = "Add file";
+            this.btnAddFile.UseVisualStyleBackColor = true;
+            this.btnAddFile.Click += new System.EventHandler(this.btnAddFile_Click);
+            // 
+            // opdAddInput
+            // 
+            this.opdAddInput.Multiselect = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(440, 359);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(458, 298);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 57);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Merge and Save";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnDleleteAll
+            // 
+            this.btnDleleteAll.Location = new System.Drawing.Point(458, 54);
+            this.btnDleleteAll.Name = "btnDleleteAll";
+            this.btnDleleteAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDleleteAll.TabIndex = 3;
+            this.btnDleleteAll.Text = "Delete all";
+            this.btnDleleteAll.UseVisualStyleBackColor = true;
+            this.btnDleleteAll.Click += new System.EventHandler(this.btnDleleteAll_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input files:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(458, 361);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MergingFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 397);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDleleteAll);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnAddFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MergingFiles";
+            this.Text = "Merging files";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddFile;
+        private System.Windows.Forms.OpenFileDialog opdAddInput;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnDleleteAll;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Merging_files/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Merging_files
+{
+    public partial class MergingFiles : Form
+    {
+        public MergingFiles()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnAddFile_Click(object sender, EventArgs e)
+        {
+         //   opdAddInput.InitialDirectory = "C:";
+            opdAddInput.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            opdAddInput.Title = "Select input files";
+            opdAddInput.Filter = "Flat files (*.TXT;*.CSV)|*.TXT;*.CSV|" + "All files (*.*)|*.*";
+            opdAddInput.ShowDialog();
+            //   MessageBox.Show(filePath);
+            foreach (String file in opdAddInput.FileNames)
+            {
+                ListViewItem item = new ListViewItem(file);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void btnDleleteAll_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string allDocText = "";
+            int i=0;
+
+            foreach (var ittem in listView1.Items)
+            {
+                StreamReader sr = new StreamReader(listView1.Items[i].Text);
+                allDocText+= sr.ReadToEnd();
+                sr.Close();
+                allDocText += "\n";
+                i++;
+            }
+
+            allDocText = allDocText.Trim('\r', '\n'); // remove empty lines on the end of file
+
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog1.Title = "Select input files";
+            saveFileDialog1.Filter = "Flat files (*.TXT;*.CSV)|*.TXT;*.CSV|" + "All files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+
+            try
+            {
+                StreamWriter plik = new StreamWriter(saveFileDialog1.FileName, false); // false to clear file before write
+                plik.WriteLine(allDocText);
+                plik.Close();
+                MessageBox.Show("Saving completed");
+
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
